ab36fa708ccb1126ec219a21fcb2013303c57bb1
Kartik Arora <kartikarora23082003@gmail.com>
2023-10-31 21:52:04 +0530

    Get weather data from OpenWeatherApi

---
+++ lib/services/weather_services.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  String apiKey = "fa7ff9c1f890d9e2ab861788a886f310";
+  final String cityName;
+
+  WeatherService({required this.cityName});
+
+  Future<dynamic> getWeatherData() async {
+    Uri url = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric");
+
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      var weatherData = jsonDecode(response.body);
+
+      return weatherData;
+    }
+  }
+}
