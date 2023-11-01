b259572d6a369b8a496f875318d4b52bbca6ebbc
Kartik Arora <kartikarora23082003@gmail.com>
2023-11-01 10:16:36 +0530

    Google fonts added

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weather_now/services/weather_services.dart';
+import 'package:weather_now/services/weather_api_call.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -12,7 +13,7 @@ class _WeatherPageState extends State<WeatherPage> {
   var textController = TextEditingController();
   double temperature = 0;
   int intTemp = 0;
-  late String cityName = "New Delhi";
+  late String cityName = "";
   late String condition = '';
 
   Future<void> setWeatherData(String userCityName) async {
@@ -56,7 +57,8 @@ class _WeatherPageState extends State<WeatherPage> {
                     ),
                     hintText: "Search City...",
                     contentPadding: EdgeInsets.all(6.0),
-                    suffixIcon: Icon(Icons.search),
+                    suffixIcon: Icon(Icons.close),
+                    prefixIcon: Icon(Icons.search),
                   ),
                 ),
               ),
@@ -64,15 +66,37 @@ class _WeatherPageState extends State<WeatherPage> {
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   cityName,
+                  style: GoogleFonts.comfortaa(
+                    color: Colors.black,
+                    //fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              Image.asset('lib/assets/icons/clouds.gif'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "$intTemp °C",
+                  style: GoogleFonts.comfortaa(
+                    color: Colors.black,
+                    fontSize: 42,
+                  ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("$intTemp"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(condition),
+                child: Text(
+                  condition,
+                  style: GoogleFonts.comfortaa(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                  ),
+                ),
               ),
             ],
           ),
